c552af332076a6f97aeb27777f0f1293dac62309
Ching-Hsin Lee <chinglee@amazon.com>
2022-08-02 19:34:08 +0800

    Add NULL pointer check in prvCheckTasksWaitingTermination

--- tasks.c
+++ tasks.c
@@ -4487,7 +4487,7 @@ static void prvCheckTasksWaitingTermination( void )
 
     #if ( INCLUDE_vTaskDelete == 1 )
     {
-        TCB_t * pxTCB;
+        TCB_t * pxTCB = NULL;
 
         /* uxDeletedTasksWaitingCleanUp is used to prevent taskENTER_CRITICAL()
          * being called too often in the idle task. */
@@ -4520,7 +4520,11 @@ static void prvCheckTasksWaitingTermination( void )
             }
             taskEXIT_CRITICAL();
 
-            prvDeleteTCB( pxTCB );
+            if( pxTCB != NULL )
+            {
+                prvDeleteTCB( pxTCB );
+                pxTCB = NULL;
+            }
         }
     }
     #endif /* INCLUDE_vTaskDelete */
