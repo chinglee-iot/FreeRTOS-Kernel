bbc69a756ffe8adb60d5ab98d8fc7c908a267ff1
Ching-Hsin Lee <chinglee@amazon.com>
2022-10-17 22:27:57 +0800

    Fix uncrustify in header file

--- include/FreeRTOS.h
+++ include/FreeRTOS.h
@@ -444,7 +444,7 @@
         #error portCHECK_IF_IN_ISR is required in SMP
     #endif
 
-#endif  /* portCHECK_IF_IN_ISR */
+#endif /* portCHECK_IF_IN_ISR */
 
 /* The timers module relies on xTaskGetSchedulerState(). */
 #if configUSE_TIMERS == 1
