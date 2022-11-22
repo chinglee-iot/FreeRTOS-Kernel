bbcf2e7270b3da8649b89ffcaf7bd1096519f808
ActoryOu <ousc@amazon.com>
2022-11-22 09:11:38 +0000

    Fix MISRA_C_RULE_10_8 violations

--- queue.c
+++ queue.c
@@ -438,7 +438,7 @@ BaseType_t xQueueGenericReset( QueueHandle_t xQueue,
                                        const uint8_t ucQueueType )
     {
         Queue_t * pxNewQueue = NULL;
-        size_t xQueueSizeInBytes;
+        UBaseType_t xQueueSizeInBytes;
         uint8_t * pucQueueStorage;
 
         if( ( uxQueueLength > ( UBaseType_t ) 0 ) &&
@@ -450,7 +450,7 @@ BaseType_t xQueueGenericReset( QueueHandle_t xQueue,
             /* Allocate enough space to hold the maximum number of items that
              * can be in the queue at any time.  It is valid for uxItemSize to be
              * zero in the case the queue is used as a semaphore. */
-            xQueueSizeInBytes = ( size_t ) ( uxQueueLength * uxItemSize ); /*lint !e961 MISRA exception as the casts are only redundant for some ports. */
+            xQueueSizeInBytes = ( UBaseType_t ) ( uxQueueLength * uxItemSize ); /*lint !e961 MISRA exception as the casts are only redundant for some ports. */
 
             /* Allocate the queue and storage area.  Justification for MISRA
              * deviation as follows:  pvPortMalloc() always ensures returned memory
